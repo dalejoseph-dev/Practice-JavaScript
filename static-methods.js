a17702223a1a07a9664735b27c4f15e57141322b
dalejoseph-dev <dalejoseph035@gmail.com>
2026-02-11 16:29:32 +0800

    Finished coding the complex problem in the static-methods.js file, And I've documented how I can solve that problem next time when I came across the same problem.

--- static-methods.js
+++ static-methods.js
@@ -1,11 +1,18 @@
 class Message {
+  static TOTAL_MESSAGES = 0;
+  static TOTAL_MESSAGE_LENGTH = 0;
   constructor(recipient, sender, body) {
     this.recipient = recipient;
     this.sender = sender;
     this.body = body;
+    Message.TOTAL_MESSAGES++;
+    Message.TOTAL_MESSAGE_LENGTH += body.length;
+  }
+  static getAverageMessageLength() {
+    const average = Message.TOTAL_MESSAGE_LENGTH / Message.TOTAL_MESSAGES; 
+    return Math.round(average * 100) / 100;
   }
 }
 
 // don't touch below this line
-
 export { Message };
