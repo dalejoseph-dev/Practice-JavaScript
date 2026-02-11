dfba2638c68d2fbac2f8e09047b92a2f47826d82
dalejoseph-dev <dalejoseph035@gmail.com>
2026-02-11 11:02:09 +0800

    Finished coding the classes.js file it's very simple to create a new class in JavaScript

--- classes.js
+++ classes.js
@@ -1,5 +1,10 @@
 class Message {
   // ?
+  constructor(recipient, sender, body) {
+    this.recipient = recipient;
+    this.sender = sender;
+    this.body = body;
+  }
 }
 
 // don't touch below this line
---
+++ private-properties.js
@@ -0,0 +1,18 @@
+class Message {
+  constructor(recipient, sender, body) {
+    this.recipient = recipient;
+    this.sender = sender;
+    this.body = body;
+    this.createdAt = new Date();
+  }
+}
+
+// don't touch below this line
+
+const message = new Message("555-1234", "555-5678", "Hi there!");
+
+console.log("Attempting to access the property createdAt...");
+console.log("createdAt: " + message.createdAt);
+
+const messageClass = Message.toString();
+console.log("has private createdAt: " + messageClass.includes("#createdAt"));
