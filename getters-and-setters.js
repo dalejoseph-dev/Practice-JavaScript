2d61cb6d815932f6c9384ccdefa0b2e2fe51e000
dalejoseph-dev <dalejoseph035@gmail.com>
2026-02-11 16:57:22 +0800

    Finished coding the getters-and-setters.js file for this lesson

--- getters-and-setters.js
+++ getters-and-setters.js
@@ -1,11 +1,19 @@
 class Contact {
   constructor(name, phoneNumber) {
     this.name = name;
-    this.phoneNumber = phoneNumber;
+    this._phoneNumber = phoneNumber;
   }
 
   // ?
-
+  set phoneNumber(value) {
+    if (value.length !== 10) {
+      throw new Error("Invalid phone number.");
+    }
+    this._phoneNumber = value;
+}
+    get phoneNumber() {
+      return `(${this._phoneNumber.slice(0, 3)}) ${this._phoneNumber.slice(3, 6)}-${this._phoneNumber.slice(6)}`;
+    }
 }
 
 // don't touch below this line
