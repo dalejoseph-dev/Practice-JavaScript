d6f796a5434eba9fa30f33a00efbdb8e73b2a5ef
dalejoseph-dev <dalejoseph035@gmail.com>
2026-02-15 22:56:01 +0800

    Finished coding the prototypal-inheritance.js still need more practice about this topic

--- prototypal-inheritance.js
+++ prototypal-inheritance.js
@@ -4,6 +4,10 @@ const notification = {
   },
 };
 
+const systemNotification = Object.create(notification);
+systemNotification.broadcast = function (message) {
+  return `Broadcast to all users: ${message}`;
+};
 // don't touch below this line
 
 export { notification, systemNotification };
