d119f1939426806cdf78f196d0678af6780ba9eb
dalejoseph-dev <dalejoseph035@gmail.com>
2026-02-18 17:53:03 +0800

    Finished coding the loops.js I got confuse a little bit on the totalCost

--- loops.js
+++ loops.js
@@ -1,5 +1,10 @@
 function bulkSendCost(numMessages) {
   // ?
+  let totalCost = 0;
+  for (let i = 0; i < numMessages; i++) {
+    totalCost += 1.0 + (0.01 * i);
+  }
+  return totalCost;
 }
 
 export { bulkSendCost };
