597b242337248f864ecb265343cbb2b39045e261
dalejoseph-dev <dalejoseph035@gmail.com>
2026-03-04 09:33:58 +0800

    Finished coding the break.js need to look into the topic of break and learn how to use it.

--- break.js
+++ break.js
@@ -1,5 +1,19 @@
 function maxMessagesWithinBudget(budget) {
   // ?
+  let totalCost = 0;
+  let count = 0;
+
+  while (true) {
+    const messageCost = 1.0 + count * 0.01;
+
+    if (totalCost + messageCost > budget) {
+      break;
+    }
+
+    totalCost += messageCost;
+    count++;
+  }
+  return count;
 }
 
 export { maxMessagesWithinBudget };
