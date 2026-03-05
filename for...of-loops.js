f05647fc241a1533da49098a43bd2a524870d788
dalejoseph-dev <dalejoseph035@gmail.com>
2026-03-05 16:51:53 +0800

    Finished coding the for...of-loops.js file it teaches me how to use toLowerCase() and for(let message of messages) keyword, still need work to understand these topics completely.

--- for...of-loops.js
+++ for...of-loops.js
@@ -1,5 +1,12 @@
 const getCleanMessages = (messages, badWord) => {
   // ?
+  const cleanMessages = [];
+  for (let message of messages) {
+    if (!message.toLowerCase().includes(badWord.toLowerCase())) {
+      cleanMessages.push(message);
+    }
+  }
+    return cleanMessages;
 };
 
 export { getCleanMessages };
