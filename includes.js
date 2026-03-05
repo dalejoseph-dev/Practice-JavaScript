f418148f02e17945c7d79d7985b00507115271f0
dalejoseph-dev <dalejoseph035@gmail.com>
2026-03-05 11:25:10 +0800

    Finished coding the includes.js file and I also added a includes to the reviewWords array.

--- includes.js
+++ includes.js
@@ -1,5 +1,24 @@
 function getCleanRank(reviewWords) {
   // ?
+  let badWordCount = 0;
+  
+  if (reviewWords.includes("dang")) {
+    badWordCount++;
+  }
+  if (reviewWords.includes("shoot")) {
+    badWordCount++;
+  }
+  if (reviewWords.includes("heck")) {
+    badWordCount++;
+  }
+  
+  if (badWordCount === 0) {
+    return "clean";
+  }
+  if (badWordCount === 1) {
+    return "dirty";
+  }
+  return "filthy";
 }
 
 export { getCleanRank };
