c17b299c9da12cb78859235795a278043d7ce3d4
dalejoseph-dev <dalejoseph035@gmail.com>
2026-03-06 11:44:19 +0800

    Done coding the slicing-arrays.js learned how to slice per string in an arrays and how to use slice().

--- slicing-arrays.js
+++ slicing-arrays.js
@@ -1,5 +1,18 @@
 function splitLogs(logs, slug) {
   // ?
+  for (let i = 0; i < logs.length; i++) {
+    if (logs[i].includes(slug))
+      return {
+      before: logs.slice(0, i),
+      after: logs.slice(i + 1),
+      i: i
+    };
+  }
+  return {
+    before: [],
+    after: [],
+    i: -1
+  };
 }
 
 export { splitLogs };
