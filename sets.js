e038415d2c358fc27c0ecdd5bfd6110a477d8515
dalejoseph-dev <dalejoseph035@gmail.com>
2026-03-12 16:56:43 +0800

    Finished coding the sets.js file and learned how to use the sets array to the code.

--- sets.js
+++ sets.js
@@ -1,5 +1,9 @@
 function deduplicateEmails(emails) {
-  // ?
+  // It teaches how to create new set and return the newly created set.
+  const uniqueSet = new Set(emails);
+  const uniqueArray = [...uniqueSet];
+  
+  return uniqueArray;
 }
 
 // don't touch below this line
