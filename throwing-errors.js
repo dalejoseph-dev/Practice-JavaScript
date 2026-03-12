5cc55ddc28e6f9811ec9f53bbce66e09df58ebf0
dalejoseph-dev <dalejoseph035@gmail.com>
2026-03-12 14:58:34 +0800

    Finished coding the throwing-errors.js learned how to throw a new error using a conditional statement and return it to the else statement block.

--- throwing-errors.js
+++ throwing-errors.js
@@ -1,5 +1,11 @@
 const sendMessage = (msg) => {
-  // ?
+  // It teaches how to throw a new error using a conditional statements and return a message on an else statement.
+  if (msg.length > 70) {
+    throw new Error("Message is too long")
+  } else {
+    return msg;
+  }
+  
 };
 
 export { sendMessage };
