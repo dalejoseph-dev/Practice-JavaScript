8207bc28195d5f6531127952cc3ed3b66389ba65
dalejoseph-dev <dalejoseph035@gmail.com>
2026-03-12 17:22:21 +0800

    Finished coding the maps.js file learned how to map a PhoneBook to a new map.

--- maps.js
+++ maps.js
@@ -1,5 +1,9 @@
 function addToPhonebook(phoneNumber, name, phoneBook) {
-  // ?
+  // It teaches how to create a new map to map a new phonebook array.
+  const newPhoneBook = new Map(phoneBook);
+  newPhoneBook.set(phoneNumber, name);
+  
+  return newPhoneBook;
 }
 
 // don't touch below this line
