2ddd5e96d163d8b09dd1635eb6aa674aca0ba3aa
dalejoseph-dev <dalejoseph035@gmail.com>
2026-03-12 11:24:57 +0800

    Finished coding the destructure.js file learned how to destructure an array.

--- destructure.js
+++ destructure.js
@@ -1,5 +1,8 @@
 function getPrimaryAndBackupMessages(messages) {
-  // ?
+  // It teaches how to use destructure array.
+  const [primary, ...backups] = messages;
+  
+  return {primary, backups};
 }
 
 export { getPrimaryAndBackupMessages };
