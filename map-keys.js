b23b79bd3b56cdf15d1497bfd86ca08271e18e74
dalejoseph-dev <dalejoseph035@gmail.com>
2026-03-13 09:59:47 +0800

    Finished coding the map-keys.js file learned how to use more on the Map() keyword.

--- map-keys.js
+++ map-keys.js
@@ -1,5 +1,11 @@
 function createUserMap(users) {
-  // ?
+  // It teaches how to use Map() keyword and learned how to use it properly.
+  const userMap = new Map();
+  for (const user of users) {
+    const fullName = `${user.fname} ${user.lname}`;
+    userMap.set(fullName, user);
+  }
+  return userMap;
 }
 
 export { createUserMap };
